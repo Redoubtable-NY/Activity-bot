c90294bd5235f4bc18d090cbb870bc4d52f8b9e6
Redoubtable-NY <Stephon.odom7@gmail.com>
2024-12-29 18:50:32 -0500

    Add set timeouts to indicate work being done in the background each tinme CTA is pressed creating new text for the subtitle and elements with id 'subtitle' and 'placeholder-text'.

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const button = document.getElementById('primary-bttn')
 const ideaContainer = document.getElementById("idea-container")
+const subtitile = document.getElementById('subtitle')
+const placeholderText = document.getElementById('placeholder-text')
+
 
 button.addEventListener("click", function() {
-    fetch("https://apis.scrimba.com/bored/api/activity")
-        .then(response => response.json())
-        .then((data) => {
-            // console.log(data)
-            return ideaContainer.innerHTML = `
-            <p id="activity">${data.activity}.</p>
-            `
-        })
+    subtitile.innerText = "Thinking..."
+    
+    function newActivityFound() {
+        return subtitile.innerText = "New actvity identified"
+    }
+
+    function placeholderLoading() {
+        return placeholderText.innerHTML= "Loading..."
+    }
+
+    setTimeout(placeholderLoading, 100)
+    
+    setTimeout(newActivityFound, 1200)
+
+    setTimeout(fetchData, 1250)
+
+    function fetchData() {
+        fetch("https://apis.scrimba.com/bored/api/activity")
+            .then(response => response.json())
+            .then((data) => {
+                // console.log(data)
+                ideaContainer.innerHTML = `
+                <p id="activity">${data.activity}.</p>
+                `
+            })
+    }
 })
