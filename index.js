f2434d7d0979ae5fe69c050f93f62fb6d91a9942
Redoubtable-NY <Stephon.odom7@gmail.com>
2024-12-30 18:11:25 -0500

    Simplify event listener on button while still indicatng a new activity is being surfaced for an individual.

--- index.js
+++ index.js
@@ -6,31 +6,20 @@ const activity = document.getElementById('activity')
 
 
 button.addEventListener("click", function() {
-    activity.innerText = ``
     subtitile.innerText = "Thinking..."
     
     function newActivityFound() {
         return subtitile.innerText = "New actvity identified"
     }
-
-    function placeholderLoading() {
-        return placeholderText.innerHTML= "A new activity is on the way..."
-    }
-
-    setTimeout(placeholderLoading, 100)
     
     setTimeout(newActivityFound, 1250)
 
-    setTimeout(fetchData, 1475)
+    setTimeout(fetchData, 1205)
 
     function fetchData() {
         fetch("https://apis.scrimba.com/bored/api/activity")
             .then(response => response.json())
             .then((data) => {
-                // console.log(data)
-                // ideaContainer.innerHTML = `
-                // <p id="activity">${data.activity}.</p>
-                // `
                 placeholderText.innerText =``
                 activity.innerText = `${data.activity}.`
             })
